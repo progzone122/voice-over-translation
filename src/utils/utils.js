03ff10a29539b785ec666952aaf5fba3878bf50d
SashaXser <24498484+SashaXser@users.noreply.github.com>
2024-06-30 01:43:50 +0400

    RegExp.exec() > String.match()
    Функция RegExp.exec() работает примерно на 20% быстрее, чем функция String.match().

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -45,14 +45,12 @@ const getVideoId = (service, video) => {
       }
 
       return (
-        url.pathname.match(/(?:watch|embed|shorts|live)\/([^/]+)/)?.[1] ||
+        /(?:watch|embed|shorts|live)\/([^/]+)/.exec(url.pathname)?.[1] ||
         url.searchParams.get("v")
       );
     }
     case "vk": {
-      const pathID = url.pathname.match(
-        /^\/(video|clip)-?[0-9]{8,9}_[0-9]{9}$/,
-      );
+      const pathID = /^\/(video|clip)-?[0-9]{8,9}_[0-9]{9}$/.exec(url.pathname);
       const paramZ = url.searchParams.get("z");
       const paramOID = url.searchParams.get("oid");
       const paramID = url.searchParams.get("id");
@@ -69,11 +67,11 @@ const getVideoId = (service, video) => {
     case "nine_gag":
     case "9gag":
     case "gag":
-      return url.pathname.match(/gag\/([^/]+)/)?.[1];
+      return /gag\/([^/]+)/.exec(url.pathname)?.[1];
     case "twitch": {
-      const clipPath = url.pathname.match(/([^/]+)\/(?:clip)\/([^/]+)/);
+      const clipPath = /([^/]+)\/(?:clip)\/([^/]+)/.exec(url.pathname);
       if (/^m\.twitch\.tv$/.test(url.hostname)) {
-        return url.href.match(/videos\/([^/]+)/)?.[0] || url.pathname.slice(1);
+        return /videos\/([^/]+)/.exec(url.href)?.[0] || url.pathname.slice(1);
       } else if (/^player\.twitch\.tv$/.test(url.hostname)) {
         return `videos/${url.searchParams.get("video")}`;
       } else if (/^clips\.twitch\.tv$/.test(url.hostname)) {
@@ -115,12 +113,12 @@ const getVideoId = (service, video) => {
         return clipPath[0];
       }
 
-      return url.pathname.match(/(?:videos)\/([^/]+)/)?.[0];
+      return /(?:videos)\/([^/]+)/.exec(url.pathname)?.[0];
     }
     case "proxitok":
-      return url.pathname.match(/([^/]+)\/video\/([^/]+)/)?.[0];
+      return /([^/]+)\/video\/([^/]+)/.exec(url.pathname)?.[0];
     case "tiktok": {
-      let id = url.pathname.match(/([^/]+)\/video\/([^/]+)/)?.[0];
+      let id = /([^/]+)\/video\/([^/]+)/.exec(url.pathname)?.[0];
       if (!id) {
         const playerEl = video.closest(".xgplayer-playing, .tiktok-web-player");
         const itemEl = playerEl?.closest(
@@ -142,29 +140,29 @@ const getVideoId = (service, video) => {
     case "vimeo": {
       const appId = url.searchParams.get("app_id");
       const videoId =
-        url.pathname.match(/[^/]+\/[^/]+$/)?.[0] ||
-        url.pathname.match(/[^/]+$/)?.[0];
+        /[^/]+\/[^/]+$/.exec(url.pathname)?.[0] ||
+        /[^/]+$/.exec(url.pathname)?.[0];
 
       return appId ? `${videoId}?app_id=${appId}` : videoId;
     }
     case "xvideos":
-      return url.pathname.match(/[^/]+\/[^/]+$/)?.[0];
+      return /[^/]+\/[^/]+$/.exec(url.pathname)?.[0];
     case "pornhub":
       return (
         url.searchParams.get("viewkey") ||
-        url.pathname.match(/embed\/([^/]+)/)?.[1]
+        /embed\/([^/]+)/.exec(url.pathname)?.[1]
       );
     case "twitter":
-      return url.pathname.match(/status\/([^/]+)/)?.[1];
+      return /status\/([^/]+)/.exec(url.pathname)?.[1];
     case "udemy":
     case "rumble":
     case "facebook":
       return url.pathname.slice(1);
     case "rutube":
-      return url.pathname.match(/(?:video|embed)\/([^/]+)/)?.[1];
+      return /(?:video|embed)\/([^/]+)/.exec(url.pathname)?.[1];
     case "coub":
       return (
-        url.pathname.match(/(?:view|embed)\/([^/]+)/)?.[1] ||
+        /(?:view|embed)\/([^/]+)/.exec(url.pathname)?.[1] ||
         document.querySelector(".coub.active")?.dataset?.permalink
       );
     case "bilibili": {
@@ -173,7 +171,7 @@ const getVideoId = (service, video) => {
         return bvid;
       }
 
-      let vid = url.pathname.match(/video\/([^/]+)/)?.[1];
+      let vid = /video\/([^/]+)/.exec(url.pathname)?.[1];
       if (vid && url.searchParams.get("p") !== null) {
         vid += `/?p=${url.searchParams.get("p")}`;
       }
@@ -186,7 +184,7 @@ const getVideoId = (service, video) => {
         return pathname.slice(1);
       }
 
-      const videoId = pathname.match(/video\/embed\/([^/]+)/)?.[1];
+      const videoId = /video\/embed\/([^/]+)/.exec(pathname)?.[1];
       if (!videoId) {
         return null;
       }
@@ -199,16 +197,16 @@ const getVideoId = (service, video) => {
       return referer?.href.split("my.mail.ru")?.[1];
     }
     case "bitchute":
-      return url.pathname.match(/(video|embed)\/([^/]+)/)?.[2];
+      return /(video|embed)\/([^/]+)/.exec(url.pathname)?.[2];
     case "coursera":
       // ! LINK SHOULD BE LIKE THIS https://www.coursera.org/learn/learning-how-to-learn/lecture/75EsZ
       // return url.pathname.match(/lecture\/([^/]+)\/([^/]+)/)?.[1]; // <--- COURSE PREVIEW
-      return url.pathname.match(/learn\/([^/]+)\/lecture\/([^/]+)/)?.[0]; // <--- COURSE PASSING (IF YOU LOGINED TO COURSERA)
+      return /learn\/([^/]+)\/lecture\/([^/]+)/.exec(url.pathname)?.[0]; // <--- COURSE PASSING (IF YOU LOGINED TO COURSERA)
     case "eporner":
       // ! LINK SHOULD BE LIKE THIS eporner.com/video-XXXXXXXXX/isdfsd-dfjsdfjsdf-dsfsdf-dsfsda-dsad-ddsd
-      return url.pathname.match(/video-([^/]+)\/([^/]+)/)?.[0];
+      return /video-([^/]+)\/([^/]+)/.exec(url.pathname)?.[0];
     case "peertube":
-      return url.pathname.match(/\/w\/([^/]+)/)?.[0];
+      return /\/w\/([^/]+)/.exec(url.pathname)?.[0];
     case "dailymotion": {
       // we work in the context of the player
       // geo.dailymotion.com
@@ -217,7 +215,7 @@ const getVideoId = (service, video) => {
       ).filter((s) => s.innerHTML.trim().includes(".m3u8"));
       try {
         let videoUrl = plainPlayerConfig[1].lastChild.src;
-        return videoUrl.match(/\/video\/(\w+)\.m3u8/)?.[1];
+        return /\/video\/(\w+)\.m3u8/.exec(videoUrl)?.[1];
       } catch (e) {
         console.error("[VOT]", e);
         return false;
@@ -229,7 +227,7 @@ const getVideoId = (service, video) => {
         return null;
       }
 
-      const path = url.pathname.match(/([^/]+)\/([\d]+)/)?.[0];
+      const path = /([^/]+)\/([\d]+)/.exec(url.pathname)?.[0];
       if (!path) {
         return null;
       }
@@ -237,38 +235,38 @@ const getVideoId = (service, video) => {
       return `${path}?vid=${vid}`;
     }
     case "yandexdisk":
-      return url.pathname.match(/\/i\/([^/]+)/)?.[1];
+      return /\/i\/([^/]+)/.exec(url.pathname)?.[1];
     case "coursehunter": {
-      const courseId = url.pathname.match(/\/course\/([^/]+)/)?.[1];
+      const courseId = /\/course\/([^/]+)/.exec(url.pathname)?.[1];
       return courseId ? courseId + url.search : false;
     }
     case "ok.ru": {
-      return url.pathname.match(/\/video\/(\d+)/)?.[1];
+      return /\/video\/(\d+)/.exec(url.pathname)?.[1];
     }
     case "googledrive":
       return url.searchParams.get("docid");
     case "bannedvideo":
       return url.searchParams.get("id");
     case "weverse":
-      return url.pathname.match(/([^/]+)\/(live|media)\/([^/]+)/)?.[0];
+      return /([^/]+)\/(live|media)\/([^/]+)/.exec(url.pathname)?.[0];
     case "newgrounds":
-      return url.pathname.match(/([^/]+)\/(view)\/([^/]+)/)?.[0];
+      return /([^/]+)\/(view)\/([^/]+)/.exec(url.pathname)?.[0];
     case "egghead":
       return url.pathname.slice(1);
     case "youku":
-      return url.pathname.match(/v_show\/id_[\w=]+/)?.[0];
+      return /v_show\/id_[\w=]+/.exec(url.pathname)?.[0];
     case "archive":
-      return url.pathname.match(/(details|embed)\/([^/]+)/)?.[2];
+      return /(details|embed)\/([^/]+)/.exec(url.pathname)?.[2];
     // case "sibnet": {
     //   const videoId = url.searchParams.get("videoid");
     //   if (videoId) {
     //     return `video${videoId}`;
     //   }
 
-    //   return url.pathname.match(/video([^/]+)/)?.[0];
+    //   return /video([^/]+)/.exec(url.pathname)?.[0];
     // }
     // case "patreon":
-    //   return url.pathname.match(/posts\/([^/]+)/)?.[0];
+    //   return /posts\/([^/]+)/.exec(url.pathname)?.[0];
     case "directlink":
       return url.pathname + url.search;
     default:
