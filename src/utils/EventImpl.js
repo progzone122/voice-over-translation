74e6052660349ed3950fc2f12b23027d6506efff
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-07-10 02:11:49 +0300

    added spaces

--- src/utils/EventImpl.js
+++ src/utils/EventImpl.js
@@ -2,9 +2,11 @@ export class EventImpl {
   constructor() {
     this.listeners = new Set();
   }
+
   hasListener(e) {
     return this.listeners.has(e);
   }
+
   dispatchToListener(handler, ...args) {
     try {
       handler(...args);
@@ -12,18 +14,21 @@ export class EventImpl {
       console.error("[VOT]", exception);
     }
   }
+
   addListener(handler) {
     if (this.hasListener(handler)) {
       throw new Error("[VOT] The listener has already been added.");
     }
     this.listeners.add(handler);
   }
+
   removeListener(handler) {
     if (!this.hasListener(handler)) {
       throw new Error("[VOT] The listener has not been added yet.");
     }
     this.listeners.delete(handler);
   }
+
   dispatch(...args) {
     for (const handler of Array.from(this.listeners)) {
       this.dispatchToListener(handler, ...args);
