2bbc3ee80dbd2edd2650ebf477a6c1d0e8464d47
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-12-16 23:45:17 +0300

    version bump

--- dist/vot-min.user.js
+++ dist/vot-min.user.js
@@ -174,7 +174,7 @@
 // @connect        speed.cloudflare.com
 // @connect        porntn.com
 // @namespace      vot-min
-// @version        1.7.2
+// @version        1.8.0
 // @icon           https://translate.yandex.ru/icons/favicon.ico
 // @author         sodapng, mynovelhost, Toil, SashaXser, MrSoczekXD
 // @homepageURL    https://github.com/ilyhalight/voice-over-translation
