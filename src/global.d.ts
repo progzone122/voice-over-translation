2ad80644bc96190ba25f72685630a60b8678e556
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-03-29 00:49:59 +0300

    refactor: moved gm types to other lib

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,34 +1,6 @@
-// DEFINED IN WEBPACK
-const DEBUG_MODE: boolean;
-const IS_BETA_VERSION: boolean;
-const AVAILABLE_LOCALES: string[];
-
-// GM API
-function GM_getValue<T = unknown>(
-  name: string,
-  def: unknown = undefined,
-): Promise<T>;
-function GM_getValue<T = unknown>(name: string, def: unknown = undefined): T;
-function GM_setValue<T = unknown>(
-  name: string,
-  value: string | boolean | number | undefined,
-): Promise<T>;
-function GM_setValue<T = unknown>(
-  name: string,
-  value: string | boolean | number | undefined,
-): T;
-function GM_deleteValue(name: string): Promise<void>;
-function GM_deleteValue(name: string): void;
-function GM_listValues(): Promise<string[]>;
-function GM_listValues(): string[];
-
-function GM_notification(
-  text: string,
-  title?: string,
-  image?: string,
-  onclick?: () => void,
-): undefined;
-
-function GM_notification(
-  options: import("./types/gm.ts").GMNotificationOptions,
-): undefined;
+declare global {
+  // DEFINED IN WEBPACK
+  const DEBUG_MODE: boolean;
+  const IS_BETA_VERSION: boolean;
+  const AVAILABLE_LOCALES: string[];
+}
